e8189b43986e5ac4d5b906549ee554feee7c5774
ngthphucs <nguyenphuch5p@gmail.com>
2025-03-31 17:11:40 +0700

    payment n analytic

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,18 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
 import { connectDB} from './lib/db.js';
+
 import authRouter from './routes/auth.route.js';
 import productRouter from './routes/product.route.js';
 import cartRouter from './routes/cart.route.js';
 import couponRouter from './routes/coupon.route.js';
-
-
+import paymentRouter from './routes/payment.route.js';
+import analyticsRoutes from './routes/analytics.route.js';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json()); // allows us to parse json data from the body of the request
 app.use(cookieParser()); // allows us to parse cookies from the request
@@ -22,6 +23,8 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/cart", cartRouter);
 app.use("/api/v1/coupons", couponRouter);
+app.use("/api/v1/payment", paymentRouter);
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(PORT, () => {
     console.log('Server is running on http://localhost:' + PORT);
